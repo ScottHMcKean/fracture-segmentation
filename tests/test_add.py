3b567e755f3adc3e1cae73b2975202b5dadf6741
Scott McKean <smckean@geoengio.com>
2020-01-16 07:12:29 -0700

    Initial commit

---
+++ tests/test_add.py
@@ -0,0 +1,5 @@
+from fracture-segmentation import add
+
+def test_my_add():
+    assert add.my_add([9, 0, 2], [0, 1]) == [9, 0, 2, 0, 1]
+    assert add.my_add('beverly ', 'hills') == 'beverly hills'
